e77b2e208c1b9bb42f88a02c86c265cf37f36d3b
María Delgado <mariad1996@gmail.com>
2024-01-11 14:11:55 +0100

    revised

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const webInfo = {
     title: 'Título de la web',
-    subtitle: 'Subtítulo de web',
+    subtitle: 'Subtítulo de la web',
     description: 'Este texto es una breve descripción de la información más relevante de la página web :)'
 }
 
