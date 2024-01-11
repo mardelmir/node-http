027c87c6c44afc06c70b54f76a94e328b6fd9e29
María Delgado <mariad1996@gmail.com>
2024-01-11 14:09:39 +0100

    finished, revise before submitting

---
+++ data.js
@@ -0,0 +1,7 @@
+const webInfo = {
+    title: 'Título de la web',
+    subtitle: 'Subtítulo de web',
+    description: 'Este texto es una breve descripción de la información más relevante de la página web :)'
+}
+
+module.exports = webInfo
---
+++ server.js
@@ -0,0 +1,26 @@
+const webInfo = require('./data.js')
+const http = require('node:http')
+
+const server = http.createServer((req, res) => {
+    console.log('Request received')
+    const template = `
+        <!DOCTYPE html>
+        <html lang="es">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Challenge: servidor HTTP</title>
+        </head>
+        <body style="text-align: center">
+            <h1>${webInfo.title}</h1>
+            <h2><i>${webInfo.subtitle}</i></h2>
+            <p>${webInfo.description}</p>
+        </body>
+        </html>`
+    res.end(template)
+})
+
+server.listen(0, ()=> {
+    console.log(`Server listening on port ${server.address().port}`)
+    console.log(`http://localhost:${server.address().port}`)
+})
